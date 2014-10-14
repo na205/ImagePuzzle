5ae475a34fd3ba02437d0b50d5480f695bc8b559
naveen <naveen@naveen-Vostro-1550.(none)>
2014-10-14 09:51:44 +0530

    first commit

---
+++ js/image-puzzle.js
@@ -0,0 +1,72 @@
+﻿
+var imagePuzzle = {
+    stepCount: 0,
+    startGame: function (images) {
+        this.setImage(images);
+        $('#playPanel').show();
+        $('#sortable').randomize();
+        this.enableSwapping('#sortable li');
+        this.stepCount = 0;
+    },
+    enableSwapping: function (elem) {
+        $(elem).draggable({
+            snap: '#droppable',
+            snapMode: 'outer',
+            revert: "invalid",
+            helper: "clone"
+        });
+        $(elem).droppable({
+            drop: function (event, ui) {
+                var $dragElem = $(ui.draggable).clone().replaceAll(this);
+                $(this).replaceAll(ui.draggable);
+
+                currentList = $('#sortable > li').map(function (i, el) { return $(el).attr('data-value'); });
+                if (isSorted(currentList))
+                    $('#actualImageBox').empty().html($('#gameOver').html());
+                else {
+                    imagePuzzle.stepCount++;
+                    $('.stepCount').text(imagePuzzle.stepCount);
+                }
+
+                imagePuzzle.enableSwapping(this);
+                imagePuzzle.enableSwapping($dragElem);
+            }
+        });
+    },
+
+    setImage: function (images) {
+        var image = images[Math.floor(Math.random() * images.length)];
+        $('#imgTitle').html(image.title);
+        $('#actualImage').attr('src', image.src);
+        $('#sortable').empty();
+        for (var i = 0; i < 16; i++) {
+            var xpos = (33.33 * (i % 4)) + '%';
+            var ypos = (33.33 * Math.floor(i / 4)) + '%';
+            var li = $('<li class="item" data-value="' + (i) + '"></li>').css({
+                'background-image': 'url(' + image.src + ')',
+                'background-position': xpos + ' ' + ypos
+            });
+            $('#sortable').append(li);
+        }
+        $('#sortable').randomize();
+    }
+};
+
+function isSorted(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        if (arr[i] != i)
+            return false;
+    }
+    return true;
+}
+$.fn.randomize = function (selector) {
+    var $elems = selector ? $(this).find(selector) : $(this).children(),
+        $parents = $elems.parent();
+
+    $parents.each(function () {
+        $(this).children(selector).sort(function () {
+            return Math.round(Math.random()) - 0.5;
+        }).remove().appendTo(this);
+    });
+    return this;
+};
